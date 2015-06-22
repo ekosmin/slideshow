05f98b8eef9c773d598337306481a41e8533b97e
Eli Kosminsky <ekosmin@mit.edu>
2015-06-22 15:38:26 -0400

    Embedding and changing video

--- Level.ts
+++ Level.ts
@@ -9,6 +9,23 @@ module Main {
 
         create() {
             this.game.stage.backgroundColor = '#777777';
+            //document.getElementById("videoWrapper").style.visibility = "hidden";
+            //document.getElementById("videoWrapper").style.height = 0;
+            var player:HTMLVideoElement = document.getElementsByTagName("video")[0];
+
+            var mp4Vid = document.getElementById('mp4Source');
+            var oggVid = document.getElementById('oggSource');
+
+            player.pause();
+
+            // Now simply set the 'src' attribute of the mp4Vid variable!!!!
+            // (...using the jQuery library in this case)
+
+            mp4Vid.setAttribute('src', 'eh5v.files/html5video/next.m4v');
+            oggVid.setAttribute('src', 'eh5v.files/html5video/next.ogv');
+
+            player.load();
+            player.play();
 
             var style: {} = {font: "32px Arial", fill: "#FFFFFF", align: "left",
                 wordWrap: true, wordWrapWidth: this.game.width};
